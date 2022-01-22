08c4de93f79c95923eb51d20b2ebdd2c85a962df
sazzad4677 <sazzad35-1898@diu.edu.bd>
2022-01-22 19:08:35 +0600

    update delete product controller and route

--- controllers/productController.js
+++ controllers/productController.js
@@ -32,3 +32,48 @@ exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
         products
     })
 })
+
+
+// Update single product => /api/v1/admin/update-product/:id
+exports.updateSingleProduct = catchAsyncErrors(async (req, res, next) => {
+    let products = await Product.exists({ productId: req.params.id });
+    if (!products) {
+        return next(new ErrorHandler("Product not found", 404))
+    }
+    products = await Product.findOneAndUpdate({ productId: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        products
+    })
+})
+
+// Delete single product => /api/v1/admin/delete-product/:id
+exports.deleteSingleProduct = catchAsyncErrors(async (req, res, next) => {
+    let products = await Product.exists({ productId: req.params.id });
+    if (!products) {
+        return next(new ErrorHandler("Product not found", 404))
+    }
+    products = await Product.deleteOne({ productId: req.params.id });
+    res.status(200).json({
+        success: true,
+        message: 'Product is deleted'
+    })
+})
+
+// Delete Multiple product => /api/v1/admin/delete-products/
+exports.deleteMultiProduct = catchAsyncErrors(async (req, res, next) => {
+    let products = await Product.exists(req.body);
+    if (!products) {
+        return next(new ErrorHandler("Product not found", 404))
+    }
+    products = await Product.deleteMany(req.body)
+    res.status(200).json({
+        success: true,
+        message: 'Product is deleted',
+    })
+})
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { newProduct, getProducts, getSingleProduct } = require('../controllers/productController')
+const { newProduct, getProducts, getSingleProduct, updateSingleProduct, deleteSingleProduct, deleteMultiProduct } = require('../controllers/productController')
 
 // add new products => /api/v1/product/new
 router.route('/product/new').post(newProduct);
@@ -9,5 +9,11 @@ router.route('/product/new').post(newProduct);
 router.route('/products').get(getProducts);
 // Get single products => /api/v1/admin/product/:id
 router.route('/admin/product/:id').get(getSingleProduct);
+// update single products => /api/v1/admin/update-product/:id
+router.route('/admin/update-product/:id').put(updateSingleProduct);
+// delete single products => /api/v1/admin/delete-product/:id
+router.route('/admin/delete-product/:id').delete(deleteSingleProduct);
+// delete multi products => /api/v1/admin/delete-products/
+router.route('/admin/delete-products').delete(deleteMultiProduct);
 
 module.exports = router;

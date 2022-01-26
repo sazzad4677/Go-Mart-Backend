c6c213f0e7791d63358c2a062e6a0d99de90bd66
sazzad4677 <sazzad35-1898@diu.edu.bd>
2022-01-26 19:09:37 +0600

    uncaughtException, unhandledRejection error added

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const app = require('./app');
+const connectDB = require('./config/database');
 const dotenv = require('dotenv');
 
-// Setting up dotenv config file
+// handle uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Error Stack: ${err.stack}`);
+    console.log(`Shutting down server due to uncaught exceptions`);
+    process.exit(1);
+})
+
+// Setting up config file
 dotenv.config({ path: './config/config.env' })
 
+
 // Connecting to database
-const connectDB = require('./config/database');
 connectDB();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+})
+
+// handle unhandled promise rejection
+process.on('unhandledRejection', err => {
+    console.log(`Error : ${err.message}`);
+    console.log(`Shutting down server due to unhandled promise rejection`);
+    server.close(() => {
+        process.exit(1);
+    });
 })
